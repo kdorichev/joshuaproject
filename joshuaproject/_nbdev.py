7322e945f2f7e004d4a07dd114f6a5cd9ca453ee
Konstantin Dorichev <kdorichev@gmail.com>
2020-01-27 17:26:04 +0300

    core -> url module

--- joshuaproject/_nbdev.py
+++ joshuaproject/_nbdev.py
@@ -2,23 +2,23 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"api_key": "00_core.ipynb",
-         "url_path_query": "00_core.ipynb",
-         "url_upgotd": "00_core.ipynb",
-         "url_upgotd.__doc__": "00_core.ipynb",
-         "url_pgs_cntry": "00_core.ipynb",
-         "url_pgs_cntry.__doc__": "00_core.ipynb",
-         "url_upgs_cntry": "00_core.ipynb",
-         "url_upgs_cntry.__doc__": "00_core.ipynb",
-         "url_pg_cntry": "00_core.ipynb",
-         "url_pg_cntry.__doc__": "00_core.ipynb",
-         "url_pg_cntries": "00_core.ipynb",
-         "url_pg_cntries.__doc__": "00_core.ipynb",
-         "url_cntry": "00_core.ipynb",
-         "url_cntry.__doc__": "00_core.ipynb",
+index = {"api_key": "00_url.ipynb",
+         "url_path_query": "00_url.ipynb",
+         "url_upgotd": "00_url.ipynb",
+         "url_upgotd.__doc__": "00_url.ipynb",
+         "url_pgs_cntry": "00_url.ipynb",
+         "url_pgs_cntry.__doc__": "00_url.ipynb",
+         "url_upgs_cntry": "00_url.ipynb",
+         "url_upgs_cntry.__doc__": "00_url.ipynb",
+         "url_pg_cntry": "00_url.ipynb",
+         "url_pg_cntry.__doc__": "00_url.ipynb",
+         "url_pg_cntries": "00_url.ipynb",
+         "url_pg_cntries.__doc__": "00_url.ipynb",
+         "url_cntry": "00_url.ipynb",
+         "url_cntry.__doc__": "00_url.ipynb",
          "CountryCodes": "01_countrycodes.ipynb"}
 
-modules = ["core.py",
+modules = ["url.py",
            "countrycodes.py"]
 
 doc_url = "https://kdorichev.github.io/joshuaproject/"
